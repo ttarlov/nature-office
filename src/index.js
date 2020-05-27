e8f3a590776a12796fad7fe9e9a298b9d6bf14df
Ed Ignot <edignot@gmail.com>
2020-05-27 16:27:09 -0600

    Importing mobx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './components/App/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react'
+import GlobalStore from './store/GlobalStore'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <Provider GlobalStore={GlobalStore}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
