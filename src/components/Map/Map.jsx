146e7f17cae2824b73ed8b1941c863e80d1a064c
Ed Ignot <edignot@gmail.com>
2020-05-27 16:51:12 -0600

    Map component added

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '100vh' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'fe5d0814-6a3f-4996-956f-f60df6c048e4' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default Map;
