ce7e1d1b2afbed25c95592448b15f292e5997f02
Madison Randle <madisonrandle25@gmail.com>
2020-05-28 11:33:56 -0500

    Update url variable and take GOOGLE out of fapiKey variable

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,12 @@
+
 const apiCalls = () => {
-  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
-  const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.837074, -97.081955&radius=50000&type=point_of_interest&key=${apiKey}`;
-  return fetch(proxyurl + url)
-  .then(response => response.json())
-  .then(contents => console.log(contents.results))
-  .catch(() => console.log("Can't access " + url + " response. Blocked by browser?"))
+const apiKey = process.env.REACT_APP_API_KEY;
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=parks&location=39.750735, -104.996520&radius=50000&type=point_of_interest&key=${apiKey}`;
+return fetch(proxyurl + url)
+.then(response => response.json())
+.then(contents => console.log(contents.results))
+.catch(() => console.log("Can't access " + url + " response. Blocked by browser?"))
 }
 
 export default apiCalls
