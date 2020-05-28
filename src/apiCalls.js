8bf440da58b22163a353c713c75a260cce92e40f
Taras Tarlov <ttarlov@gmail.com>
2020-05-28 10:57:22 -0600

    Check to export const

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 
-const apiCalls = () => {
+export const apiCalls = () => {
   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
   const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=parks&location=39.750759, -104.996656&radius=50000&type=point_of_interest&key=${apiKey}`;
@@ -8,5 +8,3 @@ const apiCalls = () => {
   .then(contents => console.log(contents.results))
   .catch(() => console.log("Can't access " + url + " response. Blocked by browser?"))
 
-
-export default apiCalls
