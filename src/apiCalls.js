1c6b15df56c67086d547191c07d3c11a3e5cc13a
Taras Tarlov <ttarlov@gmail.com>
2020-05-28 10:00:40 -0600

    Add sercure key

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,7 @@
 const apiCalls = () => {
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.837074, -97.081955&radius=50000&type=point_of_interest&key=AIzaSyDOZKvrJT1yqONNCs49PY0r6diiWC6QNdY";
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.837074, -97.081955&radius=50000&type=point_of_interest&key=${apiKey}`;
   return fetch(proxyurl + url)
   .then(response => response.json())
   .then(contents => console.log(contents.results))
