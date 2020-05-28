5aa3085a160ed7fe8e1c8691782c8f6f6d2dd7b8
Madison Randle <madisonrandle25@gmail.com>
2020-05-27 19:39:19 -0500

    Add initial route and paths

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.scss'
 import Map from '../Map/Map'
 import Login from '../Login/Login'
@@ -7,24 +7,61 @@ import Nav from '../Nav/Nav'
 import Search from '../Search/Search'
 import SpotContainer from '../SpotContainer/SpotContainer'
 import SpotDetails from '../SpotDetails/SpotDetails'
+import AddNewSpot from '../AddNewSpot/AddNewSpot'
+
 import UserPage from '../UserPage/UserPage'
 import { inject, observer } from 'mobx-react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 
+@inject('GlobalStore')
+@observer
+class App extends Component{
+  render() {
+    return (
+      <section className="App">
+        <Route
+          exact path='/' render={ () =>
+            <Login />
+          }
+        />
 
-const App = inject('GlobalStore')(observer(() => {
-  return (
-    <section className="App">
-      <Login/>
-      <LandingPage/>
-      <Map/>
-      <Nav/>
-      <Search/>
-      <SpotContainer/>
-      <SpotDetails/>
-      <UserPage/>
-    </section>
-  )
-}))
+        <Route
+          exact path='/landing' render={ () =>
+            <>
+              <Search />
+              <LandingPage />
+            </>
+          }
+        />
+
+        <Route
+          exact path='/spotContainer' render={ () =>
+            <SpotContainer />
+          }
+        />
+
+        <Route
+          exact path='/spotDetails' render={ () =>
+            <SpotDetails />
+          }
+        />
+
+        <Route
+          exact path='/userPage' render={ () =>
+            <UserPage />
+          }
+        />
+
+        <Route
+          exact path='/addNewSpot' render={ () =>
+            <AddNewSpot />
+          }
+        />
+
+        <Nav />
+      </section>
+    )
+  }
+}
 
 export default App
