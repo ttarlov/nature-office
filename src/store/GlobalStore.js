dbaa4114bea561f78fe2fa7ecdb629506db568c7
Ed Ignot <edignot@gmail.com>
2020-05-29 13:05:58 -0600

    moved static data to constants.js

--- src/store/GlobalStore.js
+++ src/store/GlobalStore.js
@@ -3,6 +3,7 @@ import { getSpotsApi, getSpotPhoto } from "../apiCalls"
 // import { RouterStore, syncHistoryWithStore } from ‘mobx-react-router’;
 import { Redirect, Link } from 'react-router-dom';
 import React from 'react'
+import { zipCodes } from '../constants'
 
 class GlobalStore {
   @observable title = 'nature office';
@@ -13,79 +14,7 @@ class GlobalStore {
   @observable userName = ''
   @observable userEmail = ''
   @observable zipCode = ''
-  @observable zipCodes = [80201
-,80202
-,80203
-,80204
-,80205
-,80210
-,80211
-,80212
-,80216
-,80217
-,80218
-,80222
-,80226
-,80227
-,80228
-,80206
-,80207
-,80208
-,80209
-,80214
-,80215
-,80219
-,80220
-,80221
-,80223
-,80224
-,80225
-,80229
-,80232
-,80233
-,80230
-,80231
-,80234
-,80236
-,80247
-,80248
-,80249
-,80256
-,80257
-,80259
-,80261
-,80262
-,80266
-,80271
-,80235
-,80237
-,80238
-,80239
-,80243
-,80244
-,80246
-,80250
-,80251
-,80252
-,80260
-,80263
-,80264
-,80265
-,80273
-,80274
-,80279
-,80280
-,80281
-,80290
-,80291
-,80293
-,80294
-,80295
-,80299
-,80294
-,80295
-,80299]
-
+  @observable zipCodes = zipCodes
 
   @action handleChange = (event) => {
     // this.loginError = ''
