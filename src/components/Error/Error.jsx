f850f19dd61ea72fd078464802df6b99852ce9e2
Taras Tarlov <ttarlov@gmail.com>
2020-05-30 12:00:40 -0600

    import globalStore

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-
-const Error = () => {
-
+import { Link } from 'react-router-dom';
+import { inject, observer } from 'mobx-react'
+import GlobalStore from '../../store/GlobalStore'
+ 
+const Error = inject('GlobalStore')(observer((props)  => {
+    // GlobalStore.errorJoke()
+    const joke = GlobalStore.joke
+    console.log(joke);
+    
 
     return (
         <div className="error-container">
             <p>404 Error this needs work to look professional</p>
+        <p>{GlobalStore.joke}</p>
+
+            <Link to='/' >
+                <button className='back-to-login-btn'>Go Back to Login Page</button>
+            </Link >
+
         </div>
     )
-}
+}))
 
 export default Error
