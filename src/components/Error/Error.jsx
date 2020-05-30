efa77d1e7f6fa08886c0febc6b83cd4a72887547
Taras Tarlov <ttarlov@gmail.com>
2020-05-30 07:56:04 -0600

    Add Error component for 404 error on client side

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+
+
+    return (
+        <div className="error-container">
+            404 Error this needs work to look professional
+        </div>
+    )
+}
+
+export default Error
