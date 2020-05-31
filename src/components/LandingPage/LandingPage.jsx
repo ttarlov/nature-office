fc4debc47746373ca7f98e41b6dd39d09dcdb820
Madison Randle <madisonrandle25@gmail.com>
2020-05-31 13:30:14 -0500

    Import Weather component

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import GlobalStore from '../../store/GlobalStore'
 import Loading from '../Loading/Loading'
+import Weather from '../Weather/Weather'
 
 const LandingPage = inject('GlobalStore')(observer(() => {
   if (!GlobalStore.spots.length || !GlobalStore.weatherTemp || !GlobalStore.weatherType) {
