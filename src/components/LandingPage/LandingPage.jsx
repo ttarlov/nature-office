8228571f3402db9eaaac507cf5750687a5054f9b
Madison Randle <madisonrandle25@gmail.com>
2020-05-31 13:18:55 -0500

    Update conditional to check for weather properties and render Loading component while fetch resolves

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,20 +3,18 @@ import { inject, observer } from 'mobx-react'
 import GlobalStore from '../../store/GlobalStore'
 import Loading from '../Loading/Loading'
 
-const LandingPage = inject('GlobalStore')(observer((props) => {
-  console.log('landingPage', GlobalStore)
-    if(!GlobalStore.spots.length) {
-      return <div>Text</div>
-    } else {
-      return (
-          <section className="landing-page">
-            <h2>HI</h2>
-            <img src={GlobalStore.spots[0].photo} alt="spot" width="20px"></img>
-            <h2>{GlobalStore.spots[0].name}</h2>
-          </section>
-        )
-    }
-
+const LandingPage = inject('GlobalStore')(observer(() => {
+  if (!GlobalStore.spots.length || !GlobalStore.weatherTemp || !GlobalStore.weatherType) {
+    return <Loading />
+  } else {
+    return (
+      <section className="landing-page">
+        <h2>LANDING PAGE</h2>
+        <img src={GlobalStore.spots[0].photo} alt="spot" width="20px"></img>
+        <h2>{GlobalStore.spots[0].name}</h2>
+      </section>
+    )
+  }
 }))
 
 export default LandingPage
