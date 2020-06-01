b9477544973d0e2c1045cab00af779f637a8b05e
Madison Randle <madisonrandle25@gmail.com>
2020-05-31 22:07:28 -0500

    Render Weather component on landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import { Link } from 'react-router-dom'
 import GlobalStore from '../../store/GlobalStore'
+import Weather from '../Weather/Weather'
 import Loading from '../Loading/Loading'
 import Search from '../Search/Search'
 
@@ -14,29 +16,38 @@ const LandingPage = inject('GlobalStore')(observer(() => {
 
   return !GlobalStore.spots.length || !GlobalStore.weatherTemp || !GlobalStore.weatherType  ?
     <Loading /> :
+      <React.Fragment>
+      <div className="weather">
+        <Weather/>
+      </div>
       <section className="landing-container">
         <Search />
-        <section className="spot-wrapper">
-          <div className="spot-img-wrapper">
-            <img
-              className="spot-img"
-              src={findRandomSpot().photo}
-              alt="spot"
-            />
-          </div>
-          <p className="category-title">Explore all</p>
-        </section>
-        <section className="spot-wrapper">
-          <div className="spot-img-wrapper">
-            <img
-              className="spot-img"
-              src={findRandomSpot().photo}
-              alt="spot"
-            />
-          </div>
-          <p className="category-title">TOP SPOTS</p>
-        </section>
+        <Link to={'/spotContainer/'}>
+          <section className="spot-wrapper">
+            <div className="spot-img-wrapper">
+              <img
+                className="spot-img"
+                src={findRandomSpot().photo}
+                alt="spot"
+              />
+            </div>
+            <p className="category-title">ALL SPOTS</p>
+          </section>
+        </Link>
+        <Link to={'/spotContainer/'}>
+          <section className="spot-wrapper">
+            <div className="spot-img-wrapper">
+              <img
+                className="spot-img"
+                src={findRandomSpot().photo}
+                alt="spot"
+              />
+            </div>
+            <p className="category-title">TOP SPOTS</p>
+          </section>
+        </Link>
       </section>
+      </React.Fragment>
 }))
 
 export default LandingPage
