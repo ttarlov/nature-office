700c1e4060a31cf190a0d1a02befc320c2b5bb80
Taras Tarlov <ttarlov@gmail.com>
2020-06-01 08:37:47 -0600

    Add localstorage mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-localstorage-mock';
+
+const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    clear: jest.fn()
+  };
+  global.localStorage = localStorageMock;
