5743346a96d21a29f727202bb086b7c8e7ad7a5b
Madison Randle <madisonrandle25@gmail.com>
2020-06-02 00:00:33 -0500

    Create Routes for searchResults, topRated, and favorites

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,75 @@
-import React, { Component } from 'react'
-import Map from '../Map/Map'
-import Login from '../Login/Login'
-import LandingPage from '../LandingPage/LandingPage'
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Map from '../Map/Map';
+import Login from '../Login/Login';
+import LandingPage from '../LandingPage/LandingPage';
 import Nav from '../Nav/Nav';
 import SpotContainer from '../SpotContainer/SpotContainer';
 import SpotDetails from '../SpotDetails/SpotDetails';
 import AddNewSpot from '../AddNewSpot/AddNewSpot';
 import GlobalStore from '../../store/GlobalStore';
 import UserPage from '../UserPage/UserPage';
-import { inject, observer } from 'mobx-react';
-import { Route, Switch, Redirect   } from 'react-router-dom';
 import Error from '../Error/Error.jsx';
-
 
 @inject('GlobalStore')
 @observer
-class App extends Component{
+class App extends Component {
   render() {
     return (
       <section className="app-container">
         <Switch>
+          <Route
+            exact path='/' render={ () => <Login /> }
+          />
+
+          {
+            GlobalStore.isFormCompleted
+            ? <Route exact path='/landing' render={ () => <LandingPage /> } />
+            : <Redirect to="/" exact />
+          }
+
+          <Route exact path='/spotContainer' render={ () =>
+            <SpotContainer
+              title={`All Spots in ${GlobalStore.city}, ${GlobalStore.zipCode}`}
+              spots={GlobalStore.spots}
+            />
+          } />
+
+          <Route exact path='/searchResults' render={ () =>
+            <SpotContainer
+              title={'Search Results'}
+              spots={GlobalStore.filteredSpots}
+            />
+          } />
+
+          <Route exact path='/topRated' render={ () =>
+            <SpotContainer
+              title={`Top Rated Spots in ${GlobalStore.city}, ${GlobalStore.zipCode}`}
+              spots={GlobalStore.filteredSpots}
+            />
+          } />
+
+          <Route exact path='/favorites' render={ () =>
+            <SpotContainer
+              title={'Favorites'}
+              spots={GlobalStore.spots}
+            />
+          } />
 
           <Route
-            exact path='/' render={ () => <Login />} />
-            {GlobalStore.isFormCompleted ?
-            <Route exact path='/landing' render={ () => <LandingPage /> }/> :
-           <Redirect to="/" exact />}
+            exact path='/spotDetails/:name' render={ () => <SpotDetails /> }
+          />
+
           <Route
-            exact path='/spotContainer' render={ () => <SpotContainer />} />
+            exact path='/userPage' render={ () => <UserPage /> }
+          />
+
           <Route
-            exact path='/spotDetails/:name' render={ () => <SpotDetails />}/>
-          <Route
-            exact path='/userPage' render={ () => <UserPage />}/>
-          <Route
-            exact path='/addNewSpot' render={ () => <AddNewSpot />}/>
-          <Route path='*'  render ={ () => <Error />}/>
+            exact path='/addNewSpot' render={ () => <AddNewSpot /> }
+          />
+
+          <Route path='*' render ={ () => <Error /> } />
         </Switch>
         {GlobalStore.spots.length > 0 && <Nav/>}
       </section>
