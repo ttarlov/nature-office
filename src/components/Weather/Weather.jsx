e71622ac0a4a300774e72a8e31bcef27c9a85633
Madison Randle <madisonrandle25@gmail.com>
2020-06-02 00:06:48 -0500

    Render the city and zip code unique to what user logsin with

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,7 +3,6 @@ import { inject, observer } from 'mobx-react'
 import GlobalStore from '../../store/GlobalStore'
 
 const Weather = inject('GlobalStore')(observer(() => {
-
   const convertToFahrenheit = () => {
     const celsiusTemp = GlobalStore.weatherTemp
     return Math.round(celsiusTemp * 9/5 + 32)
@@ -13,7 +12,7 @@ const Weather = inject('GlobalStore')(observer(() => {
     <div className="content-wrapper">
       <div className="temp-location-container">
         <p className="temp">{convertToFahrenheit()} <span>&#176;</span>F</p>
-        <p className="location">Denver</p>
+        <p className="location">{GlobalStore.city}, {GlobalStore.zipCode}</p>
       </div>
       <p className="weather-type">{GlobalStore.weatherType}</p>
     </div>
