55cd0f4ccf1a6e7f2f0c77e2298092591c3255fc
Nick Taylor <nickstaylor@gmail.com>
2020-06-01 20:36:45 -0600

    Corrected Login to show proper classNames and also render new component upon isFormCompleted = true

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,34 +8,32 @@ import homePageBackground from "./homePageBackground.jpg"
 const Login = inject('GlobalStore')(observer((props) => {
   console.log('isFormCompleted', GlobalStore.isFormCompleted)
     return (
-
-      <section className="login-form-background">
-
-    <div className="login-title">
-      <h2>{GlobalStore.title}</h2>
-      <h3 className="form-title">Find your outdoor office spot</h3>
-      </div>
-      <section className="login-form-container">
-      <form className="login-form" onSubmit = {(e)=> GlobalStore.validateUser(e)}>
-      {GlobalStore.loginError &&
-        <p className="login-error">{GlobalStore.loginError}
-        </p>}
-            <label htmlFor="userName">Name</label>
-            <input
-            type="text"
-            name="userName"
-            placeholder="name"
-            value={GlobalStore.userName}
-            onChange={GlobalStore.handleChange}
-            />
-            <label htmlFor="userEmail">Email</label>
-            <input
-            type="text"
-            name="userEmail"
-            placeholder="email"
-            value={GlobalStore.userEmail}
-            onChange={GlobalStore.handleChange}
-            />
+    <section className="login-form-background">
+        <div className="login-title">
+          <h2>{GlobalStore.title}</h2>
+          <h3 className="form-title">Find your outdoor office spot</h3>
+          </div>
+        <section className="login-form-container">
+        <form className="login-form" onSubmit = {(e)=> GlobalStore.validateUser(e)}>
+        {GlobalStore.loginError &&
+          <p className="login-error">{GlobalStore.loginError}
+          </p>}
+          <label htmlFor="userName">Name</label>
+          <input
+          type="text"
+          name="userName"
+          placeholder="name"
+          value={GlobalStore.userName}
+          onChange={GlobalStore.handleChange}
+          />
+          <label htmlFor="userEmail">Email</label>
+          <input
+          type="text"
+          name="userEmail"
+          placeholder="email"
+          value={GlobalStore.userEmail}
+          onChange={GlobalStore.handleChange}
+          />
           <label htmlFor="zipCode">Zip Code</label>
           <input
             type="text"
@@ -49,10 +47,9 @@ const Login = inject('GlobalStore')(observer((props) => {
           Go!</button>
           </form>
           {GlobalStore.isFormCompleted && <Redirect to="/landing" />}
-
         </section>
-        </section>
-      )
+    </section>
+  )
 }))
 
 export default Login
