97e742bb9c3dbffa7608434e5ecbaaad9518adc0
Madison Randle <madisonrandle25@gmail.com>
2020-06-02 09:54:00 -0500

    Create and render Favorites component

---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { inject, observer } from 'mobx-react'
+import { Link } from 'react-router-dom'
+import GlobalStore from '../../store/GlobalStore'
+import SpotContainer from '../SpotContainer/SpotContainer'
+
+const Favorites = inject('GlobalStore')(observer(() => {
+  const filteredFavorites = GlobalStore.spots.filter(spot => spot.favorite)
+  return (
+    <SpotContainer
+      title={'Your Favorites'}
+      spots={filteredFavorites}
+    />
+  )
+}))
+
+export default Favorites
