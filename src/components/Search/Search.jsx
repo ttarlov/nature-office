ce5c9faf569bed4722598f5856b589b0ec93a64a
Madison Randle <madisonrandle25@gmail.com>
2020-06-02 00:04:06 -0500

    Filter spots by search value

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,22 +1,51 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import IosSearch from 'react-ionicons/lib/IosSearch'
+import GlobalStore from '../../store/GlobalStore'
+import { Link } from 'react-router-dom'
 
 const Search = inject('GlobalStore')(observer(() => {
+  let searchInputValue
+
+  const filterSearch = () => {
+    GlobalStore.filteredSpots = GlobalStore.spots.filter(spot => {
+      return spot.name.toLowerCase().includes(searchInputValue.toLowerCase())
+    })
+  }
+
+  const updateSearchValue = (e) => {
+    searchInputValue = e.target.value;
+    searchInputValue
+    ? filterSearch()
+    : GlobalStore.filteredSpots = null;
+  }
+
+  const handleSubmit = (e) => {
+    return (
+      !GlobalStore.filteredSpots
+      ? e.preventDefault()
+      : console.log('should go to link')
+    )
+  }
+
   return (
-    <section className="search-container">
+    <form className="search-container">
       <input
         className="search-input"
         type="text"
         name="search"
-        placeholder="Search"
+        placeholder="Search by name"
+        onChange={updateSearchValue}
+        required
       />
-      <button className="search-icon">
-        <IosSearch
-          fontSize="2rem"
-        />
-      </button>
-    </section>
+      <Link
+        to={'/searchResults/'}
+        className="search-icon"
+        onClick={handleSubmit}
+      >
+        <IosSearch fontSize="2rem"/>
+      </Link>
+    </form>
   )
 }))
 
