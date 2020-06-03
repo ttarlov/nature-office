d0d2bafdfe8043f513f4870ce3f12c663425fcb8
Nick Taylor <nickstaylor@gmail.com>
2020-06-02 18:48:05 -0600

    Initial testing set-up and initial eslint clean up

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import createBrowserHistory from 'history/createBrowserHistory';
-import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
-import './index.scss';
-import App from './components/App/App.jsx';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'mobx-react'
-import GlobalStore from './store/GlobalStore'
-import { Router } from 'react-router'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import createBrowserHistory from 'history/createBrowserHistory';
+// import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+// import './index.scss';
+// import App from './components/App/App.jsx';
+// import * as serviceWorker from './serviceWorker';
+// import { Provider } from 'mobx-react'
+// import GlobalStore from './store/GlobalStore'
+// import { Router } from 'react-router'
+//
+// const browserHistory = createBrowserHistory();
+// const routingStore = new RouterStore();
+// const stores = {
+//   // Key can be whatever you want
+//   routing: routingStore,
+//   dataStore: GlobalStore
+//   // ...other stores
+// };
+//
+// const history = syncHistoryWithStore(browserHistory, routingStore);
+//
+// ReactDOM.render(
+//
+//       <Provider {...stores}>
+//     <Router history= {history}>
+//           <App />
+//     </Router>
+//         </Provider>,
+//   document.getElementById('root')
+// )
 
-const browserHistory = createBrowserHistory();
-const routingStore = new RouterStore();
-const stores = {
-  // Key can be whatever you want
-  routing: routingStore,
-  dataStore: GlobalStore
-  // ...other stores
-};
-
-const history = syncHistoryWithStore(browserHistory, routingStore);
-
-ReactDOM.render(
-
-      <Provider {...stores}>
-    <Router history= {history}>
-          <App />
-    </Router>
-        </Provider>,
-  document.getElementById('root')
-)
-
-serviceWorker.unregister();
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
@@ -46,3 +45,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
