7531f55009f19537f8aad6504895325b0d47630a
Nick Taylor <nickstaylor@gmail.com>
2020-06-02 18:47:03 -0600

    Initial testing set-up and initial eslint clean up

--- src/index.js
+++ src/index.js
@@ -31,3 +31,18 @@ ReactDOM.render(
 )
 
 serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './components/App/App.jsx'
+import * as serviceWorker from './serviceWorker'
+import { Provider } from 'mobx-react'
+import GlobalStore from './store/GlobalStore'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+  <Provider GlobalStore={GlobalStore}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
