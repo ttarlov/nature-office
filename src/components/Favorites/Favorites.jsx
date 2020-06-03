00d526f88520c3d8d297b4e703ecc63d2408e079
Nick Taylor <nickstaylor@gmail.com>
2020-06-02 18:47:28 -0600

    Initial testing set-up and initial eslint clean up

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import GlobalStore from '../../store/GlobalStore'
 import SpotContainer from '../SpotContainer/SpotContainer'
 
