736ef0c094edb7c58c39bfd5892ad1dac9367440
Nick Taylor <nickstaylor@gmail.com>
2020-06-02 18:47:16 -0600

    Initial testing set-up and initial eslint clean up

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import GlobalStore from '../../store/GlobalStore'
-import { Link, Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 
 // inject('GlobalStore')
